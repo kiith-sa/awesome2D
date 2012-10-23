7497908121cbed1ac042e92f3c2fcd8c77a49dd9
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-10-23 02:54:17 +0200

    Documented Renderer, Renderer API fixes, exceptions in separate file.

--- video/renderer.d
+++ video/renderer.d
@@ -5,8 +5,11 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
+/// Renderer API (central class of the video subsystem).
 module video.renderer;
 
+
+import std.conv;
 import std.stdio;
 
 import color;
@@ -14,152 +17,182 @@ import math.vector2;
 import image;
 
 import video.blendmode;
+import video.exceptions;
 import video.glslshader;
 import video.texture;
 import video.vertexbuffer;
 import video.indexbuffer;
 
-/// Exception thrown at renderer errors.
-class RendererException : Exception 
-{
-    public this(string msg, string file = __FILE__, int line = __LINE__)
-    {
-        super(msg, file, line);
-    }
-}
 
+/// Renderer API (central class of the video subsystem).
+///
+/// Constructs all other video API classes such as vertex buffers, textures, 
+/// etc.
 abstract class Renderer
 {
+    // TODO screenshot (for output)
+    // TODO render to texture 
+    //      (start, setting a texture, then all draws go to that texture 
+    //       through a FBO, then end)
+
     /// Constructor.
     ///
-    /// Throws: RendererException on failure.
+    /// Throws: RendererInitException on failure.
     this()
     {
     }
 
+    /// Construct an empty index buffer and return a pointer to it.
+    ///
+    /// The index buffer must be destroyed by the user before
+    /// the renderer is destroyed.
     IndexBuffer* createIndexBuffer();
 
-    VertexBuffer!V* createVertexBuffer(V)(const PrimitiveType primitiveType);
+    /// Construct an empty vertex buffer and return a pointer to it.
+    ///
+    /// Params: primitiveType = Determines which graphics primitive type 
+    ///                         should the vertices in this buffer form.
+    ///
+    /// The index buffer must be destroyed by the user before
+    /// the renderer is destroyed.
+    VertexBuffer!V* createVertexBuffer(V)(const PrimitiveType primitiveType)
+    {
+        assert(false, "TODO");
+    }
 
-    void drawVertexBuffer(V)(ref VertexBuffer!V vertexBuffer);
-
-    void drawVertexBuffer(V)(ref VertexBuffer!V vertexBuffer,
-                             ref IndexBuffer indexBuffer);
-
-    void renderFrame(void delegate() drawPartial);
-
+    /// Create a 2D texture, loading from specified image.
+    ///
+    /// The texture must be destroyed by the user before the renderer is 
+    /// destroyed.
     Texture* createTexture(const ref Image image);
 
+    /// Create a GLSL shader
     GLSLShader* createGLSLShader();
 
-    /**
-     * Are GLSL shaders supported?
-     *
-     * Shaders are required, but alternative backends might support e.g. HLSL in future.
-     */
+    /// Draw specified vertex buffer.
+    ///
+    /// The entire buffer is drawn in order of the vertices.
+    void drawVertexBuffer(V)(VertexBuffer!V* vertexBuffer)
+    {
+        assert(false, "TODO");
+    }
+
+    /// Draw specified vertex buffer with an index buffer specifying vertices to draw.
+    ///
+    /// Params: vertexBuffer = Vertex buffer to draw from.
+    ///         indexBuffer  = Index buffer specifying vertices to draw.
+    void drawVertexBuffer(V)(VertexBuffer!V* vertexBuffer, IndexBuffer* indexBuffer)
+    {
+        assert(false, "TODO");
+    }
+
+    /// A test function that draws a triangle.
+    void testDrawTriangle();
+
+    /// Render a single frame.
+    ///
+    /// Params: drawPartial = A function using the renderer to draw the scene.
+    ///                       All rendering must happen within this function.
+    ///                       If the drawPartial returns false, the frame is
+    ///                       not yet done drawing - this allows a backend
+    ///                       to split a frame when FPS is too low.
+    ///                       When the entire scene is drawn, drawPartial should
+    ///                       return true.
+    void renderFrame(bool delegate(Renderer) drawPartial);
+
+    /// Are GLSL shaders supported?
+    /// 
+    /// Shaders are required, but alternative backends might support e.g. HLSL in future.
+    /// 
     bool isGLSLSupported() const;
 
-    void setBlendMode(const BlendMode blendMode);
-
+    /// Get viewport size in pixels.
     @property Vector2u viewportSize() const;
 
+    /// Set blend mode to use for following draws.
+    void setBlendMode(const BlendMode blendMode);
+
+    /// Set video mode.
+    ///
+    /// Params: width      = Video mode width in pixels.
+    ///         height     = Video mode height in pixels.
+    ///         format     = Video mode color format.
+    ///                      Only RGB_565 and RGBA_8 are supported.
+    ///         fullscreen = Use a fullscreen video mode?
     void setVideoMode(const uint width, const uint height, 
                       const ColorFormat format, const bool fullscreen);
 
 }
 
 
-import derelict.sdl.sdl;
-
-abstract class GLRenderer : Renderer
+///Class managing lifetime and dependencies of video driver.
+class RendererContainer
 {
-protected:
-    uint screenWidth_;
-    uint screenHeight_;
-    uint screenDepth_;
+    private:
+        ///Managed renderer.
+        Renderer renderer_;
 
-protected:
-    void initGL()
-    {
-        // TODO
-    }
-
-    // TODO implement parent methods
-
-    void swapBuffers();
-}
-
-// TEMP (Until all methods are implemented)
-abstract
-class SDLGLRenderer : GLRenderer
-{
-    this()
-    {
-        writeln("Initializing SDLGLRenderer");
-        super();
-    }
-
-    ~this()
-    {
-        writeln("Destroying SDLGLRenderer");
-    }
-    
-    override void setVideoMode(const uint width, const uint height, 
-                               const ColorFormat format, const bool fullscreen)
-    {
-        assert(width >= 80 && width <= 65536, 
-               "Can't set video mode with such ridiculous width");
-        assert(height >= 60 && width <= 49152, 
-               "Can't set video mode with such ridiculous height");
-
-        //determine bit depths of color channels.
-        uint red, green, blue, alpha;
-        switch(format)
+    public:
+        /**
+         * Construct a RendererContainer.
+         *
+         * Throws:  RendererInitException on failure.
+         */
+        this()
         {
-            case ColorFormat.RGB_565:
-                red = 5;
-                green = 6;
-                blue = 5;
-                alpha = 0;
-                break;
-            case ColorFormat.RGBA_8:
-                red = 8;
-                green = 8;
-                blue = 8;
-                alpha = 8;
-                break;
-            default:
-                assert(false, "Unsupported video mode color format");
         }
 
-        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, red);
-        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, green);
-        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, blue);
-        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, alpha);
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
-        const uint bitDepth = red + green + blue + alpha;
-
-        uint flags = SDL_OPENGL;
-        if(fullscreen){flags |= SDL_FULLSCREEN;}
-
-        if(SDL_SetVideoMode(width, height, bitDepth, flags) is null)
+        /**
+         * Initialize renderer of specified type and return a reference to it.
+         *
+         * Params:  width      = Width of initial video mode.
+         *          height     = Height of initial video mode.
+         *          format     = Color format of initial video mode.
+         *          fullscreen = Should initial video mode be fullscreen?
+         *
+         * Returns: Produced renderer or null on error.
+         */
+        Renderer produce(R)(const uint width, const uint height, 
+                            const ColorFormat format, const bool fullscreen)
+            if(is(R: Renderer))
         {
-            string msg = std.string.format("Could not set video mode: %d %d %dbpp",
-                                           width, height, bitDepth);
-            writeln(msg);
-            throw new RendererException(msg);
+            auto typeString = typeid(R).toString();
+            try
+            {
+                renderer_ = new R();
+                renderer_.setVideoMode(width, height, format, fullscreen);
+            }
+            catch(RendererInitException e)
+            {
+                clear(renderer_);
+                renderer_ = null;
+                writeln("Failed to construct a " ~ typeString ~ ": " ~ e.msg);
+                return null;
+            }
+
+            return renderer_;
         }
 
-        screenWidth_  = width;
-        screenHeight_ = height;
-        screenDepth_  = bitDepth;
-        
-        initGL();
-    }
+        ///Destroy the video driver.
+        void destroy()
+        {
+            clear(renderer_);
+            renderer_ = null;
+        }
 
-    override void swapBuffers()
-    {
-        SDL_GL_SwapBuffers();
-    }
+        /**
+         * Destroy the container.
+         *
+         * Destroys any video driver dependencies.
+         * Video driver must be destroyed first by calling destroy().
+         */
+        ~this()
+        in
+        {
+            assert(renderer_ is null,
+                   "Renderer must be destroyed before its container");
+        }
+        body
+        {
+        }
 }
