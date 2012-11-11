89068303cee2d20987cbfe892fc59c11c924e85b
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-11-11 15:13:49 +0100

    Various VertexBuffer API fixes.

--- video/vertexbuffer.d
+++ video/vertexbuffer.d
@@ -10,6 +10,7 @@ module video.vertexbuffer;
 
 
 import video.gl2vertexbuffer;
+public import video.primitivetype;
 
 
 /// Vertex buffer backend struct.
@@ -19,43 +20,6 @@ import video.gl2vertexbuffer;
 /// atomic Vertex structs, the backends work on plain data (void*).
 package struct VertexBufferBackend
 {
-    union 
-    {
-        // Data storage for the GL2 backend.
-        GL2VertexBufferBackendData gl2_;
-    }
-
-    // Alias for readability.
-    alias VertexBufferBackend Self;
-
-    // Pointer to the destructor implementation.
-    void function(ref Self) dtor_;
-
-    // Pointer to addVertex implementation.
-    void function(ref Self, const void*) addVertex_;
-
-    // Pointer to lock implementation.
-    void function(ref Self) lock_;
-
-    // Pointer to unlock implementation.
-    void function(ref Self) unlock_;
-}
-
-/// Vertex buffer struct.
-///
-/// Stores vertex data for drawing.
-///
-/// Acts as a dynamic array of vertex type V.
-struct VertexBuffer(V)
-{
-package:
-    /// Backend implementation of the vertex buffer. 
-    ///
-    /// Separate because templates and function pointers/virual functions don't
-    /// work together.
-    VertexBufferBackend backend_;
-
-private:
     // Is the buffer locked (i.e. not modifiable)?
     bool locked_ = false;
 
@@ -64,6 +28,55 @@ private:
 
     // Graphics primitive type formed by the vertices of the buffer.
     PrimitiveType primitiveType_;
+
+    union 
+    {
+        // Data storage for the GL2 backend.
+        GL2VertexBufferBackendData gl2_;
+    }
+
+    // Alias for readability.
+    alias VertexBufferBackend Self;
+    // Pointer to the destructor implementation.
+    void function(ref Self) dtor_;
+    // Pointer to addVertex implementation.
+    void function(ref Self, const void*) addVertex_;
+    // Pointer to lock implementation.
+    void function(ref Self) lock_;
+    // Pointer to unlock implementation.
+    void function(ref Self) unlock_;
+}
+
+/// Vertex buffer struct.
+///
+/// Stores vertex data for drawing.
+/// Acts as a dynamic array of vertex type V.
+/// Constructed by Renderer.
+///
+/// The vertex type must have information about its attributes mixed in,
+/// using the video.vertexattribute.VertexAttributes mixin.
+///
+/// Examples:
+/// --------------------
+/// struct TestVertex
+/// {
+///     vec3 Position;
+///     vec2 TexCoord;
+/// 
+///     mixin VertexAttributes!(vec3, AttributeInterpretation.Position,
+///                             vec2, AttributeInterpretation.TexCoord);
+/// }
+/// --------------------
+///
+struct VertexBuffer(V)
+    if(is(V == struct))
+{
+package:
+    /// Backend implementation of the vertex buffer. 
+    ///
+    /// Separate because templates and function pointers/virual functions don't
+    /// work together.
+    VertexBufferBackend backend_;
 
 public:
     /// Destroy the vertex buffer.
@@ -77,9 +90,9 @@ public:
     /// Can only be called when the buffer is unlocked.
     void addVertex(ref const V vertex)
     {
-        assert(!locked_, "Trying to add a vertex to a locked vertex buffer");
-        backend_.addVertex_(cast(void*)(&backend_), vertex);
-        ++vertexCount_;
+        assert(!backend_.locked_, "Trying to add a vertex to a locked vertex buffer");
+        backend_.addVertex_(backend_, cast(void*)(&vertex));
+        ++backend_.vertexCount_;
     }
 
     /// Lock the buffer.
@@ -91,7 +104,7 @@ public:
     /// need to reupload every frame.
     void lock()
     {
-        locked_ = true;
+        backend_.locked_ = true;
         backend_.lock_(backend_);
     }
 
@@ -100,28 +113,29 @@ public:
     /// Must be called before modifying the buffer if it was locked previously.
     void unlock()
     {
-        backend_.unlock(backend_);
-        locked_ = false;
+        backend_.unlock_(backend_);
+        backend_.locked_ = false;
     } 
 
     /// Is the buffer currently locked?
-    bool locked() @safe const pure nothrow {return locked_;}
+    bool locked() @safe const pure nothrow {return backend_.locked_;}
 
     /// Get the number of vertices in the buffer.
-    uint length() @safe pure nothrow const {return vertexCount_;}
+    uint length() @safe pure nothrow const {return backend_.vertexCount_;}
 
     /// Get the graphics primitive type formed by the vertices in the buffer.
-    @property PrimitiveType primitiveType() const pure nothrow {return primitiveType_;}
+    @property PrimitiveType primitiveType() const pure nothrow {return backend_.primitiveType_;}
 
-package:
     /// Construct a VertexBuffer with specified primitive type.
+    ///
+    /// Must NOT be called directly. Use Renderer.createVertexBuffer instead.
     ///
     /// This should only be called by Renderer and optionally by its implementations.
     /// Note that the vertex buffer backend still needs to be initialized after
     /// the vertex buffer is constructed.
     this(const PrimitiveType primitiveType)
     {
-        primitiveType_ = primitiveType;
+        backend_.primitiveType_ = primitiveType;
     }
 }
 
