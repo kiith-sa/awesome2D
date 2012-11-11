d665884c2992e8a07dca0782b8b9ab27d94216bf
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-11-12 00:58:06 +0100

    Finished GL2 texture implementation.

--- video/gl2texture.d
+++ video/gl2texture.d
@@ -8,8 +8,12 @@
 module video.gl2texture;
 
 
+import std.conv;
+import std.stdio;
+
 import derelict.opengl.gl;
 import image;
+import video.exceptions;
 import video.glutils;
 import video.texture;
 
@@ -23,11 +27,99 @@ void constructTextureGL2
     texture.gl2_        = GL2TextureData.init;
     texture.dimensions_ = image.size();
     texture.params_     = params;
-    //TODO
+    texture.dtor_       = &dtor;
+    texture.bind_       = &bind;
+
+    with(texture)
+    {
+        const colorFormat = image.format;
+
+        if(!glTextureSizeSupported(dimensions_, image.format))
+        {
+            const msg = "Texture size/format combination not supported: " ~
+                        to!string(dimensions_) ~ ", " ~ to!string(colorFormat);
+            throw new TextureInitException(msg);
+        }
+
+        //TODO might be needed for loading. Once loading works, try removing.
+        glActiveTexture(GL_TEXTURE0);
+
+        glGenTextures(1, &gl2_.textureHandle_);
+        glBindTexture(GL_TEXTURE_2D, gl2_.textureHandle_);
+
+        // Set texture parameters.
+        const glFiltering = params_.filtering_.glTextureFiltering();
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, glFiltering);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, glFiltering);
+        const glWrap = params_.wrap_.glTextureWrap();
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, glWrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, glWrap);
+
+        // Texture loading parameters.
+        const internalFormat = colorFormat.glTextureInternalFormat();
+        const loadFormat     = colorFormat.glTextureLoadFormat();
+        const type           = colorFormat.glTextureType();
+
+        auto error = glGetError();
+        if(error != GL_NO_ERROR)
+        {
+            writeln("GL error before loading a texture: ", to!string(error));
+        }
+
+        // Load the texture.
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat,
+                     dimensions_.x, dimensions_.y, 0,
+                     loadFormat, type, cast(void*)image.data.ptr);
+
+        error = glGetError();
+        if(error != GL_NO_ERROR)
+        {
+            writeln("GL error after loading a texture: ", to!string(error));
+        }
+    }
 }
 
 /// Data members of the GL2 texture backend.
 struct GL2TextureData
 {
+    /// OpenGL handle to the texture.
     GLuint textureHandle_;
 }
+
+
+private:
+
+/// boundTextureUnits_[x] is the handle of the texture bound to that texture unit (if any);
+GLuint[256] boundTextures_;
+
+/// Destroy the texture.
+///
+/// Implements Texture::~this.
+void dtor(ref Texture self)
+{with(self.gl2_)
+{
+    // Not initialized.
+    // (might be called when an exception is thrown before completing initialization)
+    if(textureHandle_ == 0)
+    {
+        return;
+    }
+    // Make sure the texture is not bound to any unit.
+    foreach(unit, texture; boundTextures_)
+    {
+        assert(textureHandle_ != texture, 
+               "Trying to destroy texture " ~ to!string(textureHandle_) ~ 
+               " bound to texture unit " ~ to!string(unit));
+    }
+    glDeleteTextures(1, &textureHandle_);
+}}
+
+/// Bind the texture to specified unit.
+///
+/// Implements Texture::bind.
+void bind(ref Texture self, const uint textureUnit)
+{with(self.gl2_)
+{
+    glActiveTexture(GL_TEXTURE0 + textureUnit);
+    glBindTexture(GL_TEXTURE_2D, textureHandle_);
+}}
